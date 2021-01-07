8c1e234d3718ff7e941c711b6952e1dce67855d3
DavidNNussbaum <brocknynj@aol.com>
2021-01-07 12:45:54 -0500

    commit tree 0f5ca1bb84d08e1906a66ffe841e7a983d00bc2a

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,49 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
+    case
+    when req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
+    when req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    when req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty."
+      else
+        @@cart.each do |item|
+        resp.write "#{item}\n"
+        end
+      end
+    when req.path.match(/add/)
+      @@items.each do |item|
+      pick_item = req.params["item"]
+      if @@items.include?(pick_item)
+          @@cart << pick_item
+          resp.write "added #{pick_item}"
     else
-      resp.write "Path Not Found"
+      resp.write "We don't have that item!"
     end
-
+    end
+  end
+  
     resp.finish
   end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
+      return "Added #{search_term}"
     else
       return "Couldn't find #{search_term}"
     end
+    end
   end
-end
+
+ 
